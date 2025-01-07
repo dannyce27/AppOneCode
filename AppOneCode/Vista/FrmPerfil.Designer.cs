0d4f44482d1573f8d2acdba02d39886cb325a625
dannyce27 <danielroland27@gmail.com>
2025-01-06 22:56:51 -0600

    Cree diseño de Perfil

---
+++ AppOneCode/Vista/FrmPerfil.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace AppOneCode.Vista
+{
+    partial class FrmPerfil
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmPerfil));
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pbMinimizeForm = new System.Windows.Forms.PictureBox();
+            this.pbCloseForm = new System.Windows.Forms.PictureBox();
+            this.pbImageTrabajador = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.pictureBox9 = new System.Windows.Forms.PictureBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbMinimizeForm)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCloseForm)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImageTrabajador)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Poppins", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(360, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 48);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Perfil";
+            // 
+            // pbMinimizeForm
+            // 
+            this.pbMinimizeForm.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pbMinimizeForm.Image = global::AppOneCode.Properties.Resources.icons8_minimize_window_30;
+            this.pbMinimizeForm.Location = new System.Drawing.Point(747, 12);
+            this.pbMinimizeForm.Name = "pbMinimizeForm";
+            this.pbMinimizeForm.Size = new System.Drawing.Size(32, 29);
+            this.pbMinimizeForm.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbMinimizeForm.TabIndex = 92;
+            this.pbMinimizeForm.TabStop = false;
+            // 
+            // pbCloseForm
+            // 
+            this.pbCloseForm.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pbCloseForm.Image = global::AppOneCode.Properties.Resources.icons8_close_window_50;
+            this.pbCloseForm.Location = new System.Drawing.Point(794, 12);
+            this.pbCloseForm.Name = "pbCloseForm";
+            this.pbCloseForm.Size = new System.Drawing.Size(32, 29);
+            this.pbCloseForm.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbCloseForm.TabIndex = 91;
+            this.pbCloseForm.TabStop = false;
+            this.pbCloseForm.Click += new System.EventHandler(this.pbCloseForm_Click);
+            // 
+            // pbImageTrabajador
+            // 
+            this.pbImageTrabajador.Image = ((System.Drawing.Image)(resources.GetObject("pbImageTrabajador.Image")));
+            this.pbImageTrabajador.Location = new System.Drawing.Point(51, 144);
+            this.pbImageTrabajador.Name = "pbImageTrabajador";
+            this.pbImageTrabajador.Size = new System.Drawing.Size(162, 151);
+            this.pbImageTrabajador.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbImageTrabajador.TabIndex = 0;
+            this.pbImageTrabajador.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Poppins", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(44, 360);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(222, 37);
+            this.label2.TabIndex = 93;
+            this.label2.Text = "Nombre de Usuario";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Poppins", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(100, 404);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 37);
+            this.label3.TabIndex = 94;
+            this.label3.Text = "Gmail";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Poppins", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(69, 311);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(129, 19);
+            this.label4.TabIndex = 95;
+            this.label4.Text = "Cambiar foto de perfil";
+            // 
+            // chart1
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.chart1.Legends.Add(legend3);
+            this.chart1.Location = new System.Drawing.Point(388, 197);
+            this.chart1.Name = "chart1";
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
+            series3.Legend = "Legend1";
+            series3.Name = "Series1";
+            this.chart1.Series.Add(series3);
+            this.chart1.Size = new System.Drawing.Size(363, 171);
+            this.chart1.TabIndex = 96;
+            this.chart1.Text = "chart1";
+            // 
+            // pictureBox9
+            // 
+            this.pictureBox9.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(90)))), ((int)(((byte)(143)))));
+            this.pictureBox9.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox9.Image")));
+            this.pictureBox9.Location = new System.Drawing.Point(360, 144);
+            this.pictureBox9.Name = "pictureBox9";
+            this.pictureBox9.Size = new System.Drawing.Size(420, 243);
+            this.pictureBox9.TabIndex = 119;
+            this.pictureBox9.TabStop = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(90)))), ((int)(((byte)(143)))));
+            this.label5.Font = new System.Drawing.Font("Poppins", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(423, 156);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(293, 28);
+            this.label5.TabIndex = 120;
+            this.label5.Text = "Tareas completadas y pendientes";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(838, 684);
+            this.panel1.TabIndex = 121;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(35, 591);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(67, 69);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // FrmPerfil
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(1)))), ((int)(((byte)(37)))));
+            this.ClientSize = new System.Drawing.Size(838, 684);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.pictureBox9);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pbMinimizeForm);
+            this.Controls.Add(this.pbCloseForm);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pbImageTrabajador);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FrmPerfil";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FrmPerfil";
+            ((System.ComponentModel.ISupportInitialize)(this.pbMinimizeForm)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCloseForm)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImageTrabajador)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbImageTrabajador;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pbMinimizeForm;
+        private System.Windows.Forms.PictureBox pbCloseForm;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.PictureBox pictureBox9;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
