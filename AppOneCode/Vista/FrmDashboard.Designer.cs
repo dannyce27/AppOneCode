17c3789837fcd396cf8a3a1d6e1249f499ea795c
G3RM4NT <gonzagerman924@gmail.com>
2025-03-12 21:30:28 -0300

    a

--- AppOneCode/Vista/FrmDashboard.Designer.cs
+++ AppOneCode/Vista/FrmDashboard.Designer.cs
@@ -30,15 +30,15 @@
         {
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmDashboard));
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea5 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend5 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea6 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend6 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.panel1 = new System.Windows.Forms.Panel();
             this.btnFiltroTrabajando = new AppOneCode.Modelo.BotonPersonalizado();
             this.btnFiltroPendientes = new AppOneCode.Modelo.BotonPersonalizado();
@@ -136,7 +136,7 @@
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1300, 740);
+            this.panel1.Size = new System.Drawing.Size(1300, 787);
             this.panel1.TabIndex = 1;
             this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
@@ -471,17 +471,17 @@
             // 
             // ctTareasCompletadas
             // 
-            chartArea1.Name = "ChartArea1";
-            this.ctTareasCompletadas.ChartAreas.Add(chartArea1);
-            legend1.Name = "Legend1";
-            this.ctTareasCompletadas.Legends.Add(legend1);
+            chartArea4.Name = "ChartArea1";
+            this.ctTareasCompletadas.ChartAreas.Add(chartArea4);
+            legend4.Name = "Legend1";
+            this.ctTareasCompletadas.Legends.Add(legend4);
             this.ctTareasCompletadas.Location = new System.Drawing.Point(144, 629);
             this.ctTareasCompletadas.Name = "ctTareasCompletadas";
-            series1.ChartArea = "ChartArea1";
-            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.StackedArea;
-            series1.Legend = "Legend1";
-            series1.Name = "Series1";
-            this.ctTareasCompletadas.Series.Add(series1);
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.StackedArea;
+            series4.Legend = "Legend1";
+            series4.Name = "Series1";
+            this.ctTareasCompletadas.Series.Add(series4);
             this.ctTareasCompletadas.Size = new System.Drawing.Size(1285, 247);
             this.ctTareasCompletadas.TabIndex = 129;
             this.ctTareasCompletadas.Text = "ctTareasCompletadas";
@@ -510,16 +510,16 @@
             // 
             // ctPorcentajeProyectos
             // 
-            chartArea2.Name = "ChartArea1";
-            this.ctPorcentajeProyectos.ChartAreas.Add(chartArea2);
-            legend2.Name = "Legend1";
-            this.ctPorcentajeProyectos.Legends.Add(legend2);
+            chartArea5.Name = "ChartArea1";
+            this.ctPorcentajeProyectos.ChartAreas.Add(chartArea5);
+            legend5.Name = "Legend1";
+            this.ctPorcentajeProyectos.Legends.Add(legend5);
             this.ctPorcentajeProyectos.Location = new System.Drawing.Point(950, 130);
             this.ctPorcentajeProyectos.Name = "ctPorcentajeProyectos";
-            series2.ChartArea = "ChartArea1";
-            series2.Legend = "Legend1";
-            series2.Name = "Series1";
-            this.ctPorcentajeProyectos.Series.Add(series2);
+            series5.ChartArea = "ChartArea1";
+            series5.Legend = "Legend1";
+            series5.Name = "Series1";
+            this.ctPorcentajeProyectos.Series.Add(series5);
             this.ctPorcentajeProyectos.Size = new System.Drawing.Size(651, 391);
             this.ctPorcentajeProyectos.TabIndex = 122;
             this.ctPorcentajeProyectos.Text = "ct%Proyectos";
@@ -540,18 +540,18 @@
             // 
             this.ctEmpleadosEficientes.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(90)))), ((int)(((byte)(143)))));
             this.ctEmpleadosEficientes.BorderlineColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(90)))), ((int)(((byte)(143)))));
-            chartArea3.Name = "ChartArea1";
-            this.ctEmpleadosEficientes.ChartAreas.Add(chartArea3);
-            legend3.Name = "Legend1";
-            this.ctEmpleadosEficientes.Legends.Add(legend3);
+            chartArea6.Name = "ChartArea1";
+            this.ctEmpleadosEficientes.ChartAreas.Add(chartArea6);
+            legend6.Name = "Legend1";
+            this.ctEmpleadosEficientes.Legends.Add(legend6);
             this.ctEmpleadosEficientes.Location = new System.Drawing.Point(94, 118);
             this.ctEmpleadosEficientes.Name = "ctEmpleadosEficientes";
             this.ctEmpleadosEficientes.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.None;
-            series3.ChartArea = "ChartArea1";
-            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
-            series3.Legend = "Legend1";
-            series3.Name = "Series1";
-            this.ctEmpleadosEficientes.Series.Add(series3);
+            series6.ChartArea = "ChartArea1";
+            series6.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
+            series6.Legend = "Legend1";
+            series6.Name = "Series1";
+            this.ctEmpleadosEficientes.Series.Add(series6);
             this.ctEmpleadosEficientes.Size = new System.Drawing.Size(571, 446);
             this.ctEmpleadosEficientes.TabIndex = 120;
             this.ctEmpleadosEficientes.Text = "ctEmpleadosEficientes";
@@ -672,7 +672,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(1)))), ((int)(((byte)(37)))));
-            this.ClientSize = new System.Drawing.Size(1300, 740);
+            this.ClientSize = new System.Drawing.Size(1300, 787);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "FrmDashboard";
